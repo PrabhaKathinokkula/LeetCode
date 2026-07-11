a9a4f00128ba9e397305881725106b9f8e0991f5
Kathinokkula Bhavani Surya Prabha <kathinokkulabhavani2006@gmail.com>
2026-07-11 23:17:43 +0530

    File updated at 7/11/2026, 11:17:47 PM

--- 1050-ActorsandDirectorsWhoCooperatedAtLeastThreeTimes/1050-ActorsandDirectorsWhoCooperatedAtLeastThreeTimes.java
+++ 1050-ActorsandDirectorsWhoCooperatedAtLeastThreeTimes/1050-ActorsandDirectorsWhoCooperatedAtLeastThreeTimes.java
@@ -1,26 +1,6 @@
-// Last updated: 7/11/2026, 11:17:01 PM
+// Last updated: 7/11/2026, 11:17:47 PM
 1class Solution {
-2    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-3        ListNode dummy = new ListNode(-1);
-4        ListNode current = dummy;
-5
-6        while (list1 != null && list2 != null) {
-7            if (list1.val <= list2.val) {
-8                current.next = list1;
-9                list1 = list1.next;
-10            } else {
-11                current.next = list2;
-12                list2 = list2.next;
-13            }
-14            current = current.next;
-15        }
-16
-17        if (list1 != null) {
-18            current.next = list1;
-19        } else {
-20            current.next = list2;
-21        }
-22
-23        return dummy.next;
-24    }
-25}
+2    public int strStr(String haystack, String needle) {
+3        return haystack.indexOf(needle);
+4    }
+5}
