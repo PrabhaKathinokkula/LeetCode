fbc503650eaf458ba5e6ff53af98ed6b10bba45b
Kathinokkula Bhavani Surya Prabha <kathinokkulabhavani2006@gmail.com>
2026-07-27 00:50:10 +0530

    File updated at 7/27/2026, 12:50:10 AM

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,32 +1,17 @@
-// Last updated: 7/27/2026, 12:35:01 AM
+// Last updated: 7/27/2026, 12:50:10 AM
 1class Solution {
-2    public int[] sortedSquares(int[] nums) {
-3       int l=0;
-4       int n=nums.length;
-5       int index=n-1;
-6       int r=n-1;
-7
-8       int[] result=new int[n];
-9
-10       while(l<=r){
-11        int lsquare= nums[l]*nums[l];
-12        int rsquare=nums[r]*nums[r];
-13       
-14       if(lsquare > rsquare){
-15        result[index]= lsquare;
-16        l++;
-17       }else {
-18        result[index]= rsquare;
-19        r--;
-20       }
-21
-22       index--;
-23
-24
-25
-26     }
-27     return result;
-28        
-29       
-30    }
-31}
+2    public boolean containsDuplicate(int[] nums) {
+3    HashSet<Integer> set=new HashSet<>();
+4
+5  for(int i=0;i<nums.length;i++){
+6    if(!set.contains(nums[i]) ){
+7        set.add(nums[i]);
+8        }
+9        else{
+10            return true;
+11    
+12  }
+13    }
+14    return false;  
+15}
+16}
