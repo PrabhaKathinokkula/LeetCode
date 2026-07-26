b303494ee7a3247d031da326bac4aaddfee1008b
Kathinokkula Bhavani Surya Prabha <kathinokkulabhavani2006@gmail.com>
2026-07-27 00:35:01 +0530

    File updated at 7/27/2026, 12:35:01 AM

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,31 +1,32 @@
-// Last updated: 7/26/2026, 11:43:58 PM
+// Last updated: 7/27/2026, 12:35:01 AM
 1class Solution {
-2    public boolean isPalindrome(String s) {
-3        int l=0;
-4        int r=s.length()-1;
-5            while(l<=r){
-6                while(l<r && !Character.isLetterOrDigit(s.charAt(l)) ){
-7                    l++;
-8                  
-9                }
-10                while(l<r && !Character.isLetterOrDigit(s.charAt(r)) ){
-11                    r--;
-12                }
-13                int left=Character.toLowerCase(s.charAt(l));
-14                int right=Character.toLowerCase(s.charAt(r));
-15
-16                if(left != right){
-17                    return false;
-18                }
-19                 l++;
-20                r--;
+2    public int[] sortedSquares(int[] nums) {
+3       int l=0;
+4       int n=nums.length;
+5       int index=n-1;
+6       int r=n-1;
+7
+8       int[] result=new int[n];
+9
+10       while(l<=r){
+11        int lsquare= nums[l]*nums[l];
+12        int rsquare=nums[r]*nums[r];
+13       
+14       if(lsquare > rsquare){
+15        result[index]= lsquare;
+16        l++;
+17       }else {
+18        result[index]= rsquare;
+19        r--;
+20       }
 21
-22        
-23                 
+22       index--;
+23
 24
-25            }
-26             return true;
-27        
+25
+26     }
+27     return result;
 28        
-29    }
-30}
+29       
+30    }
+31}
